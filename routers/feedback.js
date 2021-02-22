1e66e17618d1550acf8c252acee3c31161ad0f1a
Yasir Hassan <hasanyasir01@gmail.com>
2021-02-22 13:06:55 -0800

    test

--- routers/feedback.js
+++ routers/feedback.js
@@ -3,8 +3,9 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = () => {
-  router.get('/', (req, res) => {
-    return res.send('<h1>Feedback</h`>');
-  });
+  router.get('/', (req, res) => res.send('<h1>Feedback</h1>'));
+
+  router.post('/', (req, res) => res.send('Feedback form posted'));
+
   return router;
 };
--- routers/speakers.js
+++ routers/speakers.js
@@ -3,8 +3,13 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = () => {
-  router.get('/', (req, res) => {
-    return res.send('<h1>Speaker List </h`>');
+  router.get('/', (req, res) => res.send('<h1>Speaker List </h1>'));
+
+  router.get('/:shortname', (req, res) => {
+    const { shortname } = req.params;
+
+    return res.send(`Name: ${shortname}`);
   });
+
   return router;
 };
